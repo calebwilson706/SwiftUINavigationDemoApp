4dbb2473311c2d8574581df0d6779c7141382452
Caleb Wilson <caleb.wilson@instil.co>
2022-09-20 15:39:14 +0100

    Initial Commit

---
+++ WOW/WOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  WOWApp.swift
+//  WOW
+//
+//  Created by Caleb Wilson on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
